9dba0bd63219320042e18f18853f59dfe3ab4a5c
griffen.bradds@gmail.com <griffen.bradds@gmail.com>
2018-07-27 00:53:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,45 +28,13 @@ function dec(a) {
  return decTotal
   }
 
-
-//function makeInt(a) {
-//   if (a === (a.toString())) {
-//      return a
-//   } else if (makeInt === '0x2328') {
-//     var makeIntTotalZero = parseInt('0*2328', 10)
-//     return makeIntTotalZero
-//   } else if (makeInt === 'sldkjflksjf')  {  
-//    var makeIntTotalNaN = parseInt('sldkjflksjf', 10)
-//    return makeIntTotalNaN
-//   }
-//  }
 function makeInt(a) {
   return parseInt(a, 10)
 }
 
-//function preserveDecimal(n) {
-//  if (n === '2.222') {
-//   var decimalTotal = parseFloat(n)
-//   return decimalTotal
-//  } else if (n === 'sldkjflksjf') {
-//  var decimalTotalNaN = parseFloat('sldkjflksjf')
-//  return decimalTotalNaN
-//   }
-//  }
-  
-  function preserveDecimal(n) {
-  return parseFloat(n)
-  }
+function preserveDecimal(n) {
+return parseFloat(n)
+}
 
-
-//function sayHiToGrandma(string) {
-//  if (string === string.toLowerCase()) {
-//      return "I can't hear you!"
-//  } else if (string === string.toUpperCase()) {
-//  return "YES INDEED!"
-//  } else if (string === "I love you, Grandma.") {
-//  return "I love you, too."
-//  }
-//}
 
 
